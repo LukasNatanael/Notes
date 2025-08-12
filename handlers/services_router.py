f87a876dda8e86f39f0bba9c3b3da462da7746ba
Lukas Natanael <96913773+LukasNatanael@users.noreply.github.com>
2025-08-12 15:15:39 -0300

    up: atualizado nomes de váriáveis e forma de validação do tipo de serviço

--- handlers/services_router.py
+++ handlers/services_router.py
@@ -4,9 +4,9 @@ from core.Formatar      import Formatar
 
 def router( service_name:str, service_data:str, notes:dict, extra_notes:list, infos_notes:Informations ) -> None:
 
-    service_type = service_name
+    type_service = service_name
     infos        = infos_notes
-    alarmes      = [ 'dying', 'die', 'dying_gasp', 'los', 'link_loss', 'pon' ]
+    alarmes      = [ 'dying', 'die', 'dying_gasp', 'los', 'link_loss', 'pon', 'link_los', 'link los' ]
 
     cities      = { 
         'chl':  'Conchal',
@@ -17,42 +17,42 @@ def router( service_name:str, service_data:str, notes:dict, extra_notes:list, in
         'tuju': 'Tujuguaba - Conchal'
     }
 
-    service_type = service_type.lower()
-    service      = service_data
+    type_service = type_service.lower()
+    data_service = service_data.lower()
 
-    if service_type == 'nome':
-        nome = service
-        notes['Nome'] = nome[0:25]
+    if type_service == 'nome':
+        nome = data_service
+        data_service = nome[0:25]
 
-    elif service_type == 'contato':
-        contato = service
-        notes['Contato'] =  Formatar.contato(contato)
+    elif type_service == 'contato':
+        contato = data_service
+        data_service =  Formatar.contato(contato)
 
-    elif service_type == 'cidade':
-        cidade = service
+    elif type_service == 'cidade':
+        cidade = data_service
         if cidade in cities.keys():
-            notes['Cidade'] = cities[cidade]
+            data_service = cities[cidade]
 
-    elif service in [ *alarmes, 'sem acesso' ]:
-        alarme = Validate.ask('Alarme em sistema/aparelho', message_screen=infos ,choices=alarmes) if service_type == 'sem acesso' else service
+    elif data_service in [ *alarmes, 'sem acesso' ]:
+        alarme = Validate.ask('Alarme em sistema/aparelho', message_screen=infos ,choices=alarmes) if type_service == 'sem acesso' else data_service
         
         alarme = alarme.lower()
 
-        alarme = 'DYING_GASP'   if alarme in [ 'dying', 'die', 'dying_gasp' ] else alarme
-        alarme = 'LINK_LOSS'    if alarme in [ 'link_loss' ] else alarme
-        alarme = 'LOS'          if alarme in [ 'los' ] else alarme
-        alarme = 'PON piscando' if alarme == 'pon' else alarme
-
-        tempo_sem_acesso = input(f'{alarme} há quanto tempo: ')
+        alarme = 'DYING_GASP'   if alarme in [ 'dying', 'die', 'dying_gasp' ]         else alarme
+        alarme = 'LINK_LOSS'    if alarme in [ 'link_loss', 'link_los', 'link los' ]  else alarme
+        alarme = 'LOS'          if alarme in [ 'los', 'los aceso', 'los piscando' ]   else alarme
+        alarme = 'PON piscando' if alarme in [ 'pon', 'pon piscando' ]                else alarme
+        
+        tempo_sem_acesso = input(f'{alarme} há quanto tempo: ') if alarme in [ 'DYING_GASP', 'LINK_LOSS' ] else ''
 
         alarme = alarme.replace('piscando', '').strip().lower()
-        service = f'{alarme.upper()} há {tempo_sem_acesso}' if tempo_sem_acesso else alarme
+        data_service = f'{alarme.upper()} há {tempo_sem_acesso}' if tempo_sem_acesso else alarme
         
         notes['alarme']  = alarme
         notes['tempo_sem_acesso'] = tempo_sem_acesso 
 
-    elif service in [ 'fhtt', 'mudança de endereço', 'mudança de cômodo']:
-        if service != 'mudança de cômodo':
+    elif data_service in [ 'fhtt', 'mudança de endereço', 'mudança de cômodo']:
+        if data_service != 'mudança de cômodo':
             cep = input('Informe o CEP: ')
             if cep:
                 numero_casa = Validate.ask(f'CEP: {cep} | Nº', 'Informe o número da casa!', infos)
@@ -61,7 +61,7 @@ def router( service_name:str, service_data:str, notes:dict, extra_notes:list, in
                 notes['CEP'] = Formatar.numeros(cep)
                 notes['numero_casa'] = numero_casa
     
-    elif service in ['paramount', 'watch', 'neovantagem', 'neovantagens']:
+    elif data_service in ['paramount', 'watch', 'neovantagem', 'neovantagens']:
         email   = Validate.ask('E-mail', 'Por favor, informe um e-mail!', infos).lower()
         contato = notes.get('Contato') or Validate.ask('Contato', 'Por favor, informe um contato!', infos)
         
@@ -69,7 +69,7 @@ def router( service_name:str, service_data:str, notes:dict, extra_notes:list, in
         extra_notes.append(f'E-mail: {email} | Contato: {contato} ')
         notes['email'] = email
 
-    elif service in ['vencimento']:
+    elif data_service in ['vencimento']:
         valores_planos   = [ '69.90', '84.90', '99.90', '129.90', '149.90' ]
         vencimentos      = [ '5', '10', '15', '20', '25' ]
         
@@ -85,7 +85,7 @@ def router( service_name:str, service_data:str, notes:dict, extra_notes:list, in
         extra_notes.append(f'Vencimento atual: {vencimento_atual}')
         extra_notes.append(f'Vencimento novo:  {vencimento_novo}')
 
-    elif service in [ 'lentidão', 'oscilação', 'quedas' ]:
+    elif data_service in [ 'lentidão', 'oscilação', 'quedas' ]:
         dispositivos = { 
             'wi-fi':    'Wi-Fi',
             'cabeados': 'Cabeados',
@@ -95,16 +95,22 @@ def router( service_name:str, service_data:str, notes:dict, extra_notes:list, in
 
         notes['dispositivo_em_que_ocorre'] = dispositivos[dispositivo_em_que_ocorre]
 
-    elif service in [ 'ponto adicional', 'adicional' ]:
+    elif data_service in [ 'ponto adicional', 'adicional' ]:
         local_atendimento = Validate.confirm('Feito por chat', infos)
         taxa = Validate.confirm('Taxa de instalação', infos)
         
         notes['local_atendimento'] = local_atendimento
         notes['taxa_serviço']      = taxa
 
-    service = service.upper() if service in [ 'fhtt', 'off', *alarmes ] else f'{service[0].upper()}{service[1:]}'
+    if data_service in [ 'fhtt', 'off', *alarmes ]:
+        data_service = data_service.upper()
+    elif data_service in [ 'cidade' ]:
+        data_service = f'{data_service[0].upper()}{data_service[1:]}'
+    else:
+        data_service = data_service
 
-    if service_type not in [ 'nome', 'contato', 'cidade'  ]:
-        notes['Serviço'] = service 
-        
 
+    if type_service not in [ 'nome', 'contato', 'cidade', 'chlnet' ]:
+        notes['Serviço'] = f'{data_service[0].upper()}{data_service[1:]}'
+    else:
+        notes[service_name] = data_service
